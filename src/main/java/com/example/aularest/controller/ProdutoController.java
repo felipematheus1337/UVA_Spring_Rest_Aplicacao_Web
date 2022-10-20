9fd5d57d4a4611386ebe7bcd06c04efd7ce7cc23
felipematheus1337 <lipehbr@gmail.com>
2022-10-20 17:01:00 -0300

    Parte 4 - Consultar finalizado!

--- src/main/java/com/example/aularest/controller/ProdutoController.java
+++ src/main/java/com/example/aularest/controller/ProdutoController.java
@@ -1,12 +1,16 @@
 package com.example.aularest.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.aularest.domain.model.Produto;
@@ -21,7 +25,18 @@ public class ProdutoController {
 	@GetMapping("/produtos")
 	public List<Produto> listar() {
 		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/produtos/{produtoId}")
+	public ResponseEntity<Produto> consultar(@PathVariable Integer produtoId) {
+		Optional<Produto> produto =  produtoRepository.findById(produtoId);
+		if(produto.isPresent()) {
+			return ResponseEntity.ok(produto.get());
+		}
+		return ResponseEntity.notFound().build();
+	
 		
 	}
+	
 	
 }
