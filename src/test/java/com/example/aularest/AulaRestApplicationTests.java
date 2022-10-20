4fdd761440971193ee7aafac71a19594ddba7117
felipematheus1337 <lipehbr@gmail.com>
2022-10-20 15:36:19 -0300

    projeto criado

---
+++ src/test/java/com/example/aularest/AulaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aularest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
